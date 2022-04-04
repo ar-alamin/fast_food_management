ee0842f41b9c9d0c0d0c4aa09e5a815420a10908
Abdur Rahman Al Amin <95558975+ar-alamin@users.noreply.github.com>
2022-04-04 10:18:28 +0600

    Update fast_food.sh

--- fast_food.sh
+++ fast_food.sh
@@ -1,12 +1,11 @@
-
+#!/bin/bash
 echo "------ Welcome to Fast Food Restaurant --------"
 
 echo "||....Food Name.............Size............Price......|||"
 echo "1.....Burger..............200/300g.........130tk/180tk"
-echo "2.....Chicken Pizza.......8/10incg.........120tk/158tk"
-echo "3.....French Fry.............300g...........70tk"
-echo "4.....Coke..............300/400/1000mg.....30/40/50tk"
-echo "5.....Coffee...............250g...............65tk"
+echo "2.....Chicken Pizza.......8/10inch.........120tk/170tk"
+echo "3.....French Fry.............250g...........70tk"
+echo "4.....Coffee...............250g...............65tk"
 
 echo "What do you want to order?"
 
@@ -14,20 +13,20 @@ read choice
 
 if (($choice == 1))
 then 
-echo "Which size of burger you want?\n"
-echo "1. 200g 2. 300g"
+	echo "Which size of burger you want?\n"
+	echo "1. 200g 2. 300g"
 
 read burger_size
 	if (($burger_size == 1))
 	then
-	echo "How many Burger do you want?\n"
+		echo "How many Burger do you want?\n"
 	
 	read burger_quantity
 	price= `expr $burger_quantity \* 130`
 	
 	elif ((burger_size == 2))
 	then 
-	echo "How many Burger do you want?"
+		echo "How many Burger do you want?"
 	read burger_quantity
 	price=`expr $burger_quantity \* 180`
 	fi
@@ -38,55 +37,30 @@ echo "1. 8inch 2. 10inch"
 read pizza_size
 	if (($pizza_size == 1))
 	then 
-	echo "How many Pizza do you want?"
+		echo "How many Pizza do you want?"
 	read pizza_q
 	price=`expr $pizza_q \* 120`
 	
 	elif (($pizza_size == 2))
 	then 
-	echo "How many Pizza do you want?"
+		echo "How many Pizza do you want?"
 	read pizza_q
 	price=`expr $pizza_size \* 150`
 	fi
 	
 elif ((choice == 3))
 then
-echo "How many plate of French Fries do you want?"
+	echo "How many plate of French Fries do you want?"
 read ff_q
-price=`expr ff_q \* 70`
+price=`expr $ff_q \* 70`
 
-
+	
 elif ((choice == 4))
-then
-echo "Which size Coke you want?"
-echo "1. 300ml 2. 400ml 3. 1litre"
-read coke_size
-	if (($coke_size = 1))
-	then
-	echo "How many Coke do you want?"
-	read coke_q
-	price=`expr $coke_q \* 30`
-	
-	elif (($coke_size == 2))
-	then 
-	echo "How many coke do you want?"
-	read coke_q
-	price=`expr $coke_q \* 40`
-	
-	elif (($coke_size == 3))
-	then 
-	echo "How many Coke do you want?"
-	read coke_q
-	price=`expr $coke_q \* 50`
-	fi
-	
-elif ((choice == 5))
 then 
-echo "How many Cup of Coffee do you want?"
+	echo "How many Cup of Coffee do you want?"
 read coffee_q
 price=`expr $coffee_q \* 65`
 fi
-
 
 
 echo "Do you want to order 1 more item?"
@@ -94,81 +68,56 @@ echo "1. Yes 2. No"
 read order_choice2
 
 
-
 if ((order_choice2 == 1))
 then 
-echo "What do you want to order as a second item?"
+	echo "What do you want to order as a second item?"
 
 read choice
 	if (($choice == 1))
 	then 
-	echo "Which size of burger you want?\n"
-	echo "1. 200gm 2. 300gm"
+		echo "Which size of burger you want?\n"
+		echo "1. 200gm 2. 300gm"
 	read burger_size
 		if (($burger_size == 1))
 		then
-		echo "How many Burger do you want?\n"
+			echo "How many Burger do you want?\n"
 		read burger_quantity
 		price2=`expr $burger_quantity \* 130`
 		
 		elif ((burger_size == 2))
 		then
-		echo "How many Burger do you want?"
+			echo "How many Burger do you want?"
 		read burger_quantity
 		price2=`expr $burger_quantity \* 180`
 		fi
 	elif ((choice == 2))
 	then
-	echo "Which size pizza you want?"
-	echo "1. 8inch 2. 10inch"
+		echo "Which size pizza you want?"
+		echo "1. 8inch 2. 10inch"
 	read pizza_size
 		if (($pizza_size == 1))
 		then 
-		echo "How many Pizza do you want?"
+			echo "How many Pizza do you want?"
 		read pizza_q
 		price2=`expr $pizza_q \* 120`
 		
 		elif (($pizza_size == 2))
 		then
-		echo "How many Pizza do you want?"
+			echo "How many Pizza do you want?"
 		read pizza_q
 		price1=`expr $pizza_size \* 150`
 		fi
 	
 	elif ((choice == 3))
 	then
-	echo "How many plate of Franch Fries do you want?"
+		echo "How many plate of Franch Fries do you want?"
 	read ff_q
-	price2=`expr ff_q \* 70`
+	price2=`expr $ff_q \* 70`
 	
 	
 	elif ((choice == 4))
-	then
-	echo "Which size Coke you want?"
-	echo "1. 300ml 2. 400ml 3. 1litre"
-	read coke_size
-		if (($coke_size == 1))
-		then
-		echo "How many Coke do you want?"
-		read coke_q
-		price2=`expr $coke_q \* 30`
-		
-		elif (($coke_size == 2))
-		then
-		echo "How many coke do you want?"
-		read coke_q
-		price2=`expr $coke_q \* 40`
-		
-		elif (($coke_size == 3))
-		then
-		echo "How many Coke do you want?"
-		read coke_q
-		price2=`expr $coke_q \* 50`
-		fi
-		
-	elif ((choice == 5))
 	then 
-	echo "How many cup of coffee do you want?"
+		echo "How many cup of coffee do you want?"
 	read coffee_q
 	price2=`expr $coffee_q \* 65`
 	fi
@@ -179,7 +128,7 @@ echo "Thankyou for purchasing food from our restaurant! :)"
 
 elif ((order_choice2 == 2))
 then 
-echo "Your Total Bill is:"$price
-echo "Pleasle pay the bill and wait for collecting the food!"
-echo "Thank You"
+	echo "Your Total Bill is:"$price
+	echo "Pleasle pay the bill and wait for collecting the food!"
+	echo "Thank You for purchasing food!"
 fi
